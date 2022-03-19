b13831c887a86b678a2ee8e55ff368fe869c1548
Abhisek Joshi <nofursad@gmail.com>
2022-03-19 23:51:13 +0000

    Adding the base page in template

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,11 +7,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-    debug=True)
+        debug=True)
